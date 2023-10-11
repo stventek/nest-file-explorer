08aa54e4d9461930992aaedd0074122baf477673
stventek <xenizs@protonmail.com>
2023-10-11 15:33:46 -0400

    add hierarchy endpoint

--- src/api/drive/drive.controller.ts
+++ src/api/drive/drive.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { DriveService } from './drive.service';
 import { RefreshAccessTokenGuard } from './guards/refresh-acess-token.guard';
 import { AccessToken } from './decorators/acess-token.decorator';
+import { FolderIdDto } from './dto/query.dto';
 
-@Controller('drive')
+@Controller('api/drive')
 export class DriveController {
   constructor(private readonly driveService: DriveService) {}
 
   @UseGuards(RefreshAccessTokenGuard)
   @Get('list')
-  list(@AccessToken() accessToken: string) {
-    return this.driveService.getFolderContents('test', accessToken);
+  list(@AccessToken() accessToken: string, @Query() folderIdDto: FolderIdDto) {
+    return this.driveService.getFolderHierarchy(
+      folderIdDto.folderId,
+      accessToken,
+    );
   }
 }
--- src/api/drive/drive.service.ts
+++ src/api/drive/drive.service.ts
@@ -3,20 +3,66 @@ import { google } from 'googleapis';
 
 @Injectable()
 export class DriveService {
-  list() {
-    return `This action returns all drive`;
-  }
-
-  async getFolderContents(folderPath: string, accessToken: string) {
+  async getFolderHierarchy(folderId: string, accessToken: string) {
     try {
       const auth = new google.auth.OAuth2();
       auth.setCredentials({ access_token: accessToken });
       const drive = google.drive({ version: 'v3', auth });
-      const folderResponse = await drive.files.get({
-        fileId: 'root',
-      });
 
-      return folderResponse;
+      const adjacencyList = {};
+
+      let currentFolderId = folderId;
+
+      while (currentFolderId) {
+        // Fetch folder metadata
+        const folderResponse = await drive.files.get({
+          fileId: currentFolderId,
+          fields:
+            'id, name, parents, createdTime, modifiedTime, size, mimeType',
+        });
+        // Build the adjacency list entry for the current folder
+        adjacencyList[currentFolderId] = {
+          createdAt: folderResponse.data.createdTime,
+          modified: folderResponse.data.modifiedTime,
+          name: folderResponse.data.name,
+          size: folderResponse.data.size || '0',
+          parentId: folderResponse.data.parents?.[0] || '',
+          type:
+            folderResponse.data.mimeType ===
+            'application/vnd.google-apps.folder'
+              ? 'folder'
+              : 'file',
+        };
+
+        // Fetch immediate children of the given folder
+        if (currentFolderId === folderId) {
+          const childrenResponse = await drive.files.list({
+            q: `'${currentFolderId}' in parents`,
+            fields: 'files(id, name, parents, createdTime, modifiedTime, size)',
+          });
+          // add immediate children ids to the given folder children array
+          adjacencyList[currentFolderId]['childrenIds'] =
+            childrenResponse.data.files.map((child) => child.id);
+          // add immediate children of the given folder to the adj list
+          childrenResponse.data.files.forEach((child) => {
+            adjacencyList[child.id] = {
+              createdAt: child.createdTime,
+              modified: child.modifiedTime,
+              name: child.name,
+              size: child.size,
+              parentId: folderId,
+              type:
+                child.mimeType === 'application/vnd.google-apps.folder'
+                  ? 'folder'
+                  : 'file',
+            };
+          });
+        } else {
+          adjacencyList[currentFolderId]['childrenIds'] = [];
+        }
+        currentFolderId = folderResponse.data.parents?.[0];
+      }
+      return adjacencyList;
     } catch (error) {
       throw new InternalServerErrorException(
         `Error getting folder contents ${error.message}`,
