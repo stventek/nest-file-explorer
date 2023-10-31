b0275c7b4359ab88b472dec81af521440888603b
stventek <xenizs@protonmail.com>
2023-10-31 00:40:22 -0400

    update action

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -2,16 +2,22 @@ import { Module } from '@nestjs/common';
 import * as Redis from 'redis';
 
 import { REDIS } from './redis.constants';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [
     {
       provide: REDIS,
-      useFactory: async () => {
-        const client = Redis.createClient({});
+      useFactory: async (config: ConfigService) => {
+        const password = config.get<string>('REDIS_PASSWORD');
+        const host = config.get<string>('REDIS_HOST');
+        const client = Redis.createClient({
+          url: `redis://:${password}@${host}:6379`,
+        });
         await client.connect();
         return client;
       },
+      inject: [ConfigService],
     },
   ],
   exports: [REDIS],
